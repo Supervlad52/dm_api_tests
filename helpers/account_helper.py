7d7941ec23cdd40551cf92c1c35222d90428c760
Supervlad52 <vlad.pestov52@gmail.com>
2025-08-19 15:53:42 +0300

    добавили ассерты

--- helpers/account_helper.py
+++ helpers/account_helper.py
@@ -46,7 +46,7 @@ class AccountHelper:
         self.mailhog = mailhog
 
     def auth_client(self, login: str, password: str):
-        response = self.dm_account_api.login_api.post_v1_account_login(json_data={'login': login, 'password': password})
+        response = self.user_login(login=login, password=password)
         token = {'x-dm-auth-token': response.headers['x-dm-auth-token']}
         self.dm_account_api.account_api.set_headers(token)
         self.dm_account_api.login_api.set_headers(token)
@@ -64,7 +64,10 @@ class AccountHelper:
         }
         response = self.dm_account_api.account_api.post_v1_account(json_data=json_data)
         assert response.status_code == 201, f"Пользователь не был создан {response.json()}"
+        start_time = time.time()
         token = self.get_activation_token_by_login(login=login)
+        end_time = time.time()
+        assert end_time - start_time < 3, "Время ожидания активации превышено"
         assert token is not None, f"Токен для пользователя {login} не был получен"
         response = self.dm_account_api.account_api.put_v1_account_token(token=token)
         assert response.status_code == 200, "Пользователь не был активирован"
@@ -82,6 +85,7 @@ class AccountHelper:
             'rememberMe': remember_me,
         }
         response = self.dm_account_api.login_api.post_v1_account_login(json_data=json_data)
+        assert response.headers["x-dm-auth-token"], "Токен для пользователя не был получен"
         assert response.status_code == 200, "Пользователь не смог авторизоваться"
         return response
 
