920a785f4c1bf7e6839213558ab77ca5dd2cfff9
Supervlad52 <vlad.pestov52@gmail.com>
2025-08-25 17:15:57 +0300

    добавили checker_functions

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -10,9 +10,9 @@ def test_get_v1_account_auth(auth_account_helper):
     # with check_status_code_http(200, 'User must be authenticated'):
     response = auth_account_helper.dm_account_api.account_api.get_v1_account()
     with soft_assertions():
-        # assert_that(response.resource.login).is_equal_to('pestov_test')
-        # assert_that(response.resource.online).is_equal_to(datetime)
-        assert_that(response.resource.roles).contains(UserRole.GUEST, UserRole.PLAYER)
+        assert_that(response.resource.login).is_equal_to('pestov_test')
+        assert_that(response.resource.online).is_equal_to(datetime)
+        assert_that(response.resource.roles).contains(UserRole.Guest, UserRole.Player)
 
 
 def test_get_v1_account_no_auth(account_helper):
--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,14 +1,4 @@
-from datetime import datetime
-
-from hamcrest import (
-    assert_that,
-    has_property,
-    starts_with,
-    all_of,
-    instance_of,
-    has_properties,
-    equal_to,
-)
+from checkers.post_v1_account import PostV1Account
 
 
 def test_post_v1_account(
@@ -20,23 +10,7 @@ def test_post_v1_account(
     email = prapare_user.email
     account_helper.register_new_user(login=login, password=password, email=email)
     response = account_helper.user_login(login=login, password=password, validate_response=True)
-    assert_that(
-        response, all_of(
-            has_property('resource', has_property('login', starts_with('pestov_test'))),
-            has_property('resource', has_property('registration', instance_of(datetime))),
-            has_property(
-                'resource', has_properties(
-                    {
-                        'rating': has_properties(
-                                {
-                                    "enabled": equal_to(True),
-                                    "quality": equal_to(0),
-                                    "quantity": equal_to(0)
-                                }
-                        )
-                    }
-                )
-            )
-        )
-    )
-    print(response)
+    PostV1Account.check_response_values(response)
+
+
+
