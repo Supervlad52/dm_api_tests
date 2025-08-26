324a77aa6ea51d150178408c7566dad14039e17e
Supervlad52 <vlad.pestov52@gmail.com>
2025-08-26 15:01:42 +0300

    добавили конфигурацию

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,16 @@ from json import loads
 
 import pytest
 
+from pathlib import Path
 from helpers.account_helper import AccountHelper
 from restclient.configuration import Configuration as MaihogConfiguration
 from restclient.configuration import Configuration as DmApiConfiguration
 from services.dm_api_account import DMApiAccount
 from services.api_mailhog import MailHogApi
+from vyper import (
+    vyper,
+    v
+)
 
 import structlog
 
@@ -22,17 +27,42 @@ structlog.configure(
     ]
 )
 
+options = (
+    "service.dm_api_account",
+    'service.mailhog',
+    'user.login',
+    'user.password',
+)
+
+
+@pytest.fixture(scope='session', autouse=True)
+def set_config(request):
+    config = Path(__file__).joinpath("../../").joinpath("config")
+    config_name = request.config.getoption('--env')
+    v.set_config_name(config_name)
+    v.add_config_path(config)
+    v.read_in_config()
+    for option in options:
+        v.set(f'{option}', request.config.getoption(f"--{option}"))
+
+
+def pytest_addoption(parser):
+    parser.addoption('--env', action='store', default='stg', help='run stg')
+
+    for option in options:
+        parser.addoption(f'--{option}', action="store", default=None)
+
 
 @pytest.fixture(scope='session')
 def mailhog_api():
-    mailhog_configuration = MaihogConfiguration(host='http://5.63.153.31:5025')
+    mailhog_configuration = MaihogConfiguration(host=v.get('service.mailhog'), disable_log=False)
     mailhog_client = MailHogApi(configuration=mailhog_configuration)
     return mailhog_client
 
 
 @pytest.fixture(scope='session')
 def account_api():
-    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
+    dm_api_configuration = DmApiConfiguration(host=v.get('service.dm_api_account'), disable_log=False)
     account = DMApiAccount(configuration=dm_api_configuration)
     return account
 
@@ -48,12 +78,12 @@ def account_helper(
 
 @pytest.fixture(scope='session')
 def auth_account_helper(mailhog_api):
-    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
+    dm_api_configuration = DmApiConfiguration(host=v.get('service.dm_api_account'), disable_log=False)
     account = DMApiAccount(configuration=dm_api_configuration)
     account_helper = AccountHelper(mailhog=mailhog_api, dm_account_api=account)
     account_helper.auth_client(
-        login='pestov_test',
-        password='123456789'
+        login=v.get('user.login'),
+        password=v.get('user.password')
     )
     return account_helper
 
@@ -64,7 +94,7 @@ def prapare_user():
     data = now.strftime("%d_%m_%Y_%H_%M_%S")
     login = f'pestov_test_{data}'
     email = f'{login}@mail.ru'
-    password = '123456789'
+    password = v.get('user.password')
     User = namedtuple("User", ["login", "password", "email"])
     user = User(login=login, password=password, email=email)
     return user
--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
 from checkers.http_checkers import check_status_code_http
-from assertpy import assert_that, soft_assertions
+from assertpy import soft_assertions
+from hamcrest import assert_that
 
 from dm_api_account.models.user_envelope import UserRole
 
@@ -10,9 +11,9 @@ def test_get_v1_account_auth(auth_account_helper):
     # with check_status_code_http(200, 'User must be authenticated'):
     response = auth_account_helper.dm_account_api.account_api.get_v1_account()
     with soft_assertions():
-        assert_that(response.resource.login).is_equal_to('pestov_test')
+        # assert_that(response.resource.login).is_equal_to('pestov_test')
         assert_that(response.resource.online).is_equal_to(datetime)
-        assert_that(response.resource.roles).contains(UserRole.Guest, UserRole.Player)
+        assert_that(response.resource.roles).contains(UserRole.GUEST, UserRole.PLAYER)
 
 
 def test_get_v1_account_no_auth(account_helper):
