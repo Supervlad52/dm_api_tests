85b4cabf9d89ea10c641eb8a794d446bbd820835
Supervlad52 <vlad.pestov52@gmail.com>
2025-09-03 14:32:07 +0300

    report_5

--- packages/notifier/bot.py
+++ packages/notifier/bot.py
@@ -1,9 +1,10 @@
+import os
 from pathlib import Path
 from telebot import TeleBot
 from vyper import v
 
 config = Path(__file__).parent.joinpath("../../").joinpath("config")
-v.set_config_name('prod')
+v.set_config_name('stg')
 v.add_config_path(config)
 v.read_in_config()
 
@@ -23,6 +24,8 @@ v.read_in_config()
 def send_file() -> None:
     telegram_bot = TeleBot(v.get('telegram.token'))
     file_path = Path(__file__).parent.joinpath('../../').joinpath("swagger-coverage-dm-api-account.html")
+    print("Looking for file at:", file_path)
+    print("File exists:", os.path.exists(file_path))
     with open(file_path, 'rb') as document:
         telegram_bot.send_document(
             v.get('telegram.chat_id'),
