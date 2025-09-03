e2e70ac903ddf8cd721a324cf60a874be5e4a17b
Supervlad52 <vlad.pestov52@gmail.com>
2025-09-03 14:39:43 +0300

    report_7

--- tests/conftest.py
+++ tests/conftest.py
@@ -43,12 +43,22 @@ options = (
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_swagger_coverage():
-    reporter = CoverageReporter(api_name="dm-api-account", host="http://5.63.153.31:5051")
-    reporter.cleanup_input_files()
-    reporter.setup("/swagger/Account/swagger.json")
-    yield
-    reporter.generate_report()
-    reporter.cleanup_input_files()
+    # reporter = CoverageReporter(api_name="dm-api-account", host="http://5.63.153.31:5051")
+    # reporter.cleanup_input_files()
+    # reporter.setup("/swagger/Account/swagger.json")
+    # yield
+    # reporter.generate_report()
+    # reporter.cleanup_input_files()
+    try:
+        reporter = CoverageReporter(api_name="dm-api-account", host="http://5.63.153.31:5051 ")
+        reporter.cleanup_input_files()
+        reporter.setup("/swagger/Account/swagger.json")
+        yield
+        reporter.generate_report()
+        reporter.cleanup_input_files()
+    except Exception as e:
+        print("Swagger coverage failed:", e)
+        yield
 
 
 @pytest.fixture(scope='session', autouse=True)
